4736dda57d43334289a5cd7ab11fde4d1dd70143
tonylex12 <you@example.com>
2021-05-03 10:00:26 -0500

    Homework finished

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,20 +5,16 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if (x > y) {
-    return x;
-  }
-    return y;
+  if (x > y) return x;
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-  if (edad >= 18) {
-    return 'Allowed';
-  }
-    return 'Not allowed';
+  if (edad >= 18) return 'Allowed'
+  return 'Not allowed';
 }
   
 function conection(status) {
@@ -27,13 +23,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-  if (status === 1) {
-    return 'Online';
-  }
-  else if (status === 2) {
-    return 'Away';
-  }
-    return 'Offline';
+  if (status === 1) return 'Online'
+  if (status === 2) return 'Away';
+  return 'Offline';
 }
 
 function saludo(idioma) {
