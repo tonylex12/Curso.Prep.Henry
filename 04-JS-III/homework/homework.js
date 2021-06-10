aa7a92eef7254b9a390bf75375134184dccdb7c5
Antony Huerto <tonylex12@gmail.com>
2021-06-10 18:41:07 -0500

    Practicando

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,12 +3,14 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 
